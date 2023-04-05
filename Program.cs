b0131aa2b04d3282b8b026ccc1114788eb4d2647
doxxing-teb <115967395+doxxing-teb@users.noreply.github.com>
2023-04-05 13:18:04 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿Console.WriteLine("Autor: Agustin Ibarra");
+Console.WriteLine("Lisencia: GNU General Public V3.0");
+
+Console.WriteLine("Hola, Digame Su Altura");
+        string Altura = Console.ReadLine();
+    
+switch (Altura)
+{
+    case "190":
+        Console.WriteLine("Usted Es Alto");
+    break;
+    case "189":
+        Console.WriteLine("Usted Es Alto");
+    break;
+    case "188":
+        Console.WriteLine("Usted Es Alto");
+    break;
+     case "187":
+        Console.WriteLine("Usted Es Alto");
+    break;
+     case "186":
+        Console.WriteLine("Usted Es Alto");
+    break;
+    case "185":
+        Console.WriteLine("Usted Es Alto");
+    break;
+     case "184":
+        Console.WriteLine("Usted Es Alto");
+    break;
+     case "183":
+        Console.WriteLine("Usted Es Alto");
+    break;
+     case "182":
+        Console.WriteLine("Usted Es Alto");
+    break;
+     case "181":
+        Console.WriteLine("Usted Es Alto");
+    break;
+    case "180":
+        Console.WriteLine("Usted Es Algo Alto");
+    break;
+    case "179":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "178":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "177":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "176":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "175":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "174":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "173":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "172":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "171":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "170":
+        Console.WriteLine("Usted Es Normal");
+    break;
+    case "169":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "168":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "167":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "166":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "165":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "164":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "163":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "162":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "161":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "160":
+        Console.WriteLine("Usted Es Alguien bajo");
+    break;
+    case "159":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "158":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "157":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "156":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "155":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "154":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "153":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "152":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "151":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "150":
+        Console.WriteLine("Usted Es Alguien Bajito");
+    break;
+    case "149":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+    case "148":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+    case "147":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+    case "146":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+    case "145":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+    case "144":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+    case "143":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+    case "142":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+    case "141":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+    case "140":
+        Console.WriteLine("Uy, Perdon, Casi lo piso");
+    break;
+}
+
+Console.WriteLine("Gracias Por Usar El Medidor De Altura Por Terminal :)");
